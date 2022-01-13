e309ea6c8b2e09d6334b67ccb63b5551a1649e11
GauravBegig <gaurav.bajaj@begig.io>
2022-01-13 14:54:11 +0530

    new changes undo from app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
 import "./SCSS/index.scss";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useHistory,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Admin/Login";
 import SignUp from "./components/Admin/Signup";
 import RecentPublications from "./components/Admin/RecentPublications";
@@ -15,108 +10,27 @@ import Brands from "./components/brands/brands";
 import AddBrands from "./components/brands/Dialogs/AddBrands";
 import AddVehicle from "./components/Vehicle/VehicleApplication";
 import VehicleStepper from "./components/Vehicle/VehicleStepper";
-import Sidebar from "./components/common/sidebar";
-import StoreApplication from "./components/Store/StoreApplication";
-import Store from "./components/Store/Store.js";
+import Home from "./components/Admin/home/Home";
 function App() {
-  const history = useHistory();
-  console.log(window.location.pathname === "/");
-  const Routes = [
-    {
-      path: "/",
-      exact: true,
-      main: () => <Login />,
-    },
-    {
-      path: "/signup",
-      main: () => <SignUp />,
-    },
-    {
-      path: "/recentpublication",
-      main: () => <RecentPublications />,
-    },
-    {
-      path: "/vehicleapplication",
-      main: () => <VehicleTable />,
-    },
-    {
-      path: "/vehicle",
-      main: () => <Vehicle />,
-    },
-    {
-      path: "/brands",
-      main: () => <Brands />,
-    },
-    {
-      path: "/addbrands",
-      main: () => <AddBrands />,
-    },
-    {
-      path: "/addvehicle",
-      main: () => <AddVehicle />,
-    },
-    {
-      path: "/vehiclestepper",
-      main: () => <VehicleStepper />,
-    },
-    {
-      path: "/storeApplication",
-      main: () => <StoreApplication />,
-    },
-    {
-      path: "/store",
-      main: () => <Store />,
-    },
-  ];
-
   return (
     <>
       <Router>
-        <div
-          style={{
-            display:
-              window.location.pathname == "/" ||
-              window.location.pathname == "/signup"
-                ? ""
-                : "flex",
-          }}
-        >
-          <div>
-            {window.location.pathname == "/" ||
-            window.location.pathname == "/signup" ? (
-              ""
-            ) : (
-              <Sidebar />
-            )}
-          </div>
-          <div>
-            <div
-              style={{
-                flex:
-                  window.location.pathname == "/" ||
-                  window.location.pathname == "/signup"
-                    ? ""
-                    : 1,
-                padding:
-                  window.location.pathname == "/" ||
-                  window.location.pathname == "/signup"
-                    ? ""
-                    : "10px",
-              }}
-            >
-              <Switch>
-                {Routes.map((route, index) => (
-                  <Route
-                    key={index}
-                    path={route.path}
-                    exact={route.exact}
-                    children={<route.main />}
-                  />
-                ))}
-              </Switch>
-            </div>
-          </div>
-        </div>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route
+            exact
+            path="/recentpublication"
+            component={RecentPublications}
+          />
+          <Route exact path="/vehicleapplication" component={VehicleTable} />
+          <Route exact path="/vehicle" component={Vehicle} />
+          <Route exact path="/brands" component={Brands} />
+          <Route exact path="/addbrands" component={AddBrands} />
+          <Route exact path="/addvehicle" component={AddVehicle} />
+          <Route exact path="/vehiclestepper" component={VehicleStepper} />
+          {/* <Route exact path="/home" component={Home} /> */}
+        </Switch>
       </Router>
     </>
   );
--- src/components/Admin/home/Home.js
+++ src/components/Admin/home/Home.js
@@ -1,11 +1,79 @@
-import React from 'react'
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { faker } from "faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  console.log(faker.datatype.number);
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Chart.js Line Chart",
+      },
+    },
+  };
 
-export default Home
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: labels.map(() =>
+          faker.datatype.number({ min: -1000, max: 1000 })
+        ),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Dataset 2",
+        data: labels.map(() =>
+          faker.datatype.number({ min: -1000, max: 1000 })
+        ),
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Line options={options} data={data} />
+    </div>
+  );
+};
+
+export default Home;
